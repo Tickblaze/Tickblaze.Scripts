9b70b3f05515ff758d807913d13b6f9830933a8e
sbgtrading <sbgtradingcorp@gmail.com>
2024-10-23 09:12:51 -0400

    Final version - merged from branch "tpdn-1623-AnchoredVWAP-drawingtool"

--- Tickblaze.Scripts/Drawings/AnchoredVWAP.cs
+++ Tickblaze.Scripts/Drawings/AnchoredVWAP.cs
@@ -1,13 +1,7 @@
 ﻿
-using Tickblaze.Scripts.Indicators;
-
-namespace Tickblaze.Scripts.Drawings;
 
 public sealed class AnchoredVWAP : Drawing
 {
-	[Parameter("Extend to current bar")]
-	public bool ExtendToCurrentBar { get; set; } = false;
-
 	[Parameter("VWAP Line Color")]
 	public Color VWAPLineColor { get => _bandSettingsDict[VWAPIds.VWAP].Color; set => _bandSettingsDict[VWAPIds.VWAP].Color = value; }
 
@@ -18,7 +12,7 @@ public sealed class AnchoredVWAP : Drawing
 	public LineStyle VWAPLineStyle { get => _bandSettingsDict[VWAPIds.VWAP].LineStyle; set => _bandSettingsDict[VWAPIds.VWAP].LineStyle = value; }
 
 	[Parameter("Band 1 deviations"), NumericRange(0, double.MaxValue)]
-	public double Band1Mult { get => _bandSettingsDict[VWAPIds.Band1].Multiplier; set => _bandSettingsDict[VWAPIds.Band1].Multiplier = value; }
+	public double Band1Multiplier { get => _bandSettingsDict[VWAPIds.Band1].Multiplier; set => _bandSettingsDict[VWAPIds.Band1].Multiplier = value; }
 
 	[Parameter("Band 1 Color")]
 	public Color Band1Color { get => _bandSettingsDict[VWAPIds.Band1].Color; set => _bandSettingsDict[VWAPIds.Band1].Color = value; }
@@ -30,7 +24,7 @@ public sealed class AnchoredVWAP : Drawing
 	public LineStyle Band1LineStyle { get => _bandSettingsDict[VWAPIds.Band1].LineStyle; set => _bandSettingsDict[VWAPIds.Band1].LineStyle = value; }
 
 	[Parameter("Band 2 deviations"), NumericRange(0, double.MaxValue)]
-	public double Band2Mult { get => _bandSettingsDict[VWAPIds.Band2].Multiplier; set => _bandSettingsDict[VWAPIds.Band2].Multiplier = value; }
+	public double Band2Multiplier { get => _bandSettingsDict[VWAPIds.Band2].Multiplier; set => _bandSettingsDict[VWAPIds.Band2].Multiplier = value; }
 
 	[Parameter("Band 2 Color")]
 	public Color Band2Color { get => _bandSettingsDict[VWAPIds.Band2].Color; set => _bandSettingsDict[VWAPIds.Band2].Color = value; }
@@ -42,7 +36,7 @@ public sealed class AnchoredVWAP : Drawing
 	public LineStyle Band2LineStyle { get => _bandSettingsDict[VWAPIds.Band2].LineStyle; set => _bandSettingsDict[VWAPIds.Band2].LineStyle = value; }
 
 	[Parameter("Band 3 deviations"), NumericRange(0, double.MaxValue)]
-	public double Band3Mult { get => _bandSettingsDict[VWAPIds.Band3].Multiplier; set => _bandSettingsDict[VWAPIds.Band3].Multiplier = value; }
+	public double Band3Multiplier { get => _bandSettingsDict[VWAPIds.Band3].Multiplier; set => _bandSettingsDict[VWAPIds.Band3].Multiplier = value; }
 
 	[Parameter("Band 3 Color")]
 	public Color Band3Color { get => _bandSettingsDict[VWAPIds.Band3].Color; set => _bandSettingsDict[VWAPIds.Band3].Color = value; }
@@ -53,22 +47,14 @@ public sealed class AnchoredVWAP : Drawing
 	[Parameter("Band 3 Line Style")]
 	public LineStyle Band3LineStyle { get => _bandSettingsDict[VWAPIds.Band3].LineStyle; set => _bandSettingsDict[VWAPIds.Band3].LineStyle = value; }
 
-	[Parameter("Anchor line Color")]
-	public Color AnchorLineColor { get; set; } = Color.DimGray;
-
-	[Parameter("Anchor Line Thickness"), NumericRange(0, 5)]
-	public int AnchorLineThickness { get; set; } = 1;
-
-	[Parameter("Anchor Line Style")]
-	public LineStyle AnchorLineStyle { get; set; } = LineStyle.Solid;
-
-	public override int PointsCount => 2;
+	public override int PointsCount => 1;
 
 	private readonly Dictionary<VWAPIds, BandSettings> _bandSettingsDict = new()
 	{
 		[VWAPIds.VWAP] = new BandSettings
 		{
-			Color = Color.Cyan
+			Color = Color.Cyan,
+			Thickness = 1
 		},
 		[VWAPIds.Band1] = new BandSettings
 		{
@@ -96,83 +82,64 @@ public sealed class AnchoredVWAP : Drawing
 	private readonly Dictionary<VWAPIds, double> _priorUpperY = [];
 	public override void OnRender(IDrawingContext context)
 	{
-		if (AnchorLineThickness > 0)
-		{
-			context.DrawLine(Points[0], Points[1], AnchorLineColor, AnchorLineThickness, AnchorLineStyle);
+		var leftIndex = 0;
+		var rightIndex = 0;
+		var validBarIndexes = CalculateLeftAndRightIndexes(ref leftIndex, ref rightIndex, Points[0].X);
 
-			//foreach (var pt in Points)
-			//{
-			//	var isHigh = pt == Points[0];
-			//	var bar = Chart.GetBarIndexByXCoordinate(Points[0].X);
-			//	context.DrawText(Points[0], $"{(isHigh ? "High" : "Low")} @ {bar}: {Bars.Symbol.FormatPrice(isHigh ? Bars[bar].High : Bars[bar].Low)}  time: {Bars[bar].Time.ToLocalTime()}", Color.White);
-			//}
-		}
-
-		var leftIndex = Math.Max(0, Math.Min(Bars.Count - 1, Chart.GetBarIndexByXCoordinate(Math.Min(Points[0].X, Points[1].X))));
-		var lastBarXCoord = Chart.GetXCoordinateByBarIndex(Bars.Count - 1);
-		var indexOfRighmostPoint = Chart.GetBarIndexByXCoordinate(Math.Max(Points[0].X, Points[1].X));
-		//NOTE:  GetBarIndexByXCoordinate() returns -1 if the X coord exceeds the X of the rightmost bar
-		if (indexOfRighmostPoint == -1)
-		{
-			indexOfRighmostPoint = Bars.Count - 1;
-		}
-		var rightmostXCoord = Chart.GetXCoordinateByBarIndex(indexOfRighmostPoint);
-		var rightIndex = Math.Max(1, ExtendToCurrentBar ? Bars.Count - 1 : indexOfRighmostPoint);
-
-		var volumeSum = 0.0;
-		var typicalVolumeSum = 0.0;
-		var varianceSum = 0.0;
-		var pointL = new Point(0, 0);
-		var pointR = new Point(0, 0);
-
-		if (leftIndex >= rightIndex - 1)
+		if (validBarIndexes == null || validBarIndexes.Length == 0 || leftIndex >= rightIndex - 1 || Points[0].X > Chart.GetXCoordinateByBarIndex(Bars.Count - 1))
 		{
 			return;
 		}
 
-		// Gap bars are null
-		var validBarIndexes = Enumerable.Range(leftIndex, rightIndex - leftIndex)
-			.Where(i => Bars[i] != null)
-			.ToArray();
-		context.DrawText(Points[0], $" Bars.Count {Bars.Count}  left index {leftIndex}   Chart.LastVisBarIndex {Chart.LastVisibleBarIndex}  validBarIndexesMax {validBarIndexes.Max()}", Color.White);
+		var volumeSum = 0.0;
+		var typicalVolumeSum = 0.0;
+		var varianceSum = 0.0;
+		var vwapPointLeft = new Point(0, 0);
+		var vwapPointRight = new Point(0, 0);
 
 		for (var i = 0; i < validBarIndexes.Length; i++)
 		{
 			var barIndex = validBarIndexes[i];
 			var bar = Bars[barIndex];
+			var volume = bar.Volume;
 			var typicalPrice = (bar.High + bar.Low + bar.Close) / 3;
-			typicalVolumeSum += bar.Volume * typicalPrice;
+			typicalVolumeSum += volume * typicalPrice;
 
 			if (i == 0)
 			{
 				volumeSum = bar.Volume;
-				pointR = new Point(Chart.GetXCoordinateByBarIndex(barIndex), ChartScale.GetYCoordinateByValue(typicalPrice));
+				vwapPointRight = new Point(Chart.GetXCoordinateByBarIndex(barIndex), ChartScale.GetYCoordinateByValue(typicalPrice));
+				//update the Y value of the anchor point only if necessary - has performance gains
+				if (Points[0].Y != vwapPointRight.Y)
+				{
+					Points[0].Y = vwapPointRight.Y;
+				}
 
 				//left-edge of all plots start out at the same Y pixel
 				foreach (var id in Enum.GetValues<VWAPIds>())
 				{
-					_priorUpperY[id] = _priorLowerY[id] = pointR.Y;
+					_priorUpperY[id] = _priorLowerY[id] = vwapPointRight.Y;
 				}
 
 				continue;
 			}
 
-			volumeSum += bar.Volume;
+			volumeSum += volume;
 			var curVWAP = typicalVolumeSum / volumeSum;
 			var diff = typicalPrice - curVWAP;
 			varianceSum += diff * diff;
 			var deviation = Math.Sqrt(Math.Max(varianceSum / (i + 1), 0));
 
 			//left-edge X pixel is set to the last print X pixel
-			pointL.X = pointR.X;
-			pointR.X = Chart.GetXCoordinateByBarIndex(barIndex);
+			vwapPointLeft.X = vwapPointRight.X;
+			vwapPointRight.X = Chart.GetXCoordinateByBarIndex(barIndex);
 
 			foreach (var id in Enum.GetValues<VWAPIds>())
 			{
-				pointL.Y = _priorUpperY[id];
-				pointR.Y = ChartScale.GetYCoordinateByValue(curVWAP + deviation * _bandSettingsDict[id].Multiplier);
-				_priorUpperY[id] = pointR.Y;
-				context.DrawLine(pointL, pointR, _bandSettingsDict[id].Color, _bandSettingsDict[id].Thickness, _bandSettingsDict[id].LineStyle);
+				vwapPointLeft.Y = _priorUpperY[id];
+				vwapPointRight.Y = ChartScale.GetYCoordinateByValue(curVWAP + deviation * _bandSettingsDict[id].Multiplier);
+				_priorUpperY[id] = vwapPointRight.Y;
+				context.DrawLine(vwapPointLeft, vwapPointRight, _bandSettingsDict[id].Color, _bandSettingsDict[id].Thickness, _bandSettingsDict[id].LineStyle);
 
 				//Draw the lower line plot only if this is band 1, 2 or 3
 				if (id == VWAPIds.VWAP)
@@ -180,14 +147,35 @@ public sealed class AnchoredVWAP : Drawing
 					continue;
 				}
 
-				pointL.Y = _priorLowerY[id];
-				pointR.Y = ChartScale.GetYCoordinateByValue(curVWAP - deviation * _bandSettingsDict[id].Multiplier);
-				_priorLowerY[id] = pointR.Y;
-				context.DrawLine(pointL, pointR, _bandSettingsDict[id].Color, _bandSettingsDict[id].Thickness, _bandSettingsDict[id].LineStyle);
+				vwapPointLeft.Y = _priorLowerY[id];
+				vwapPointRight.Y = ChartScale.GetYCoordinateByValue(curVWAP - deviation * _bandSettingsDict[id].Multiplier);
+				_priorLowerY[id] = vwapPointRight.Y;
+				context.DrawLine(vwapPointLeft, vwapPointRight, _bandSettingsDict[id].Color, _bandSettingsDict[id].Thickness, _bandSettingsDict[id].LineStyle);
 			}
 		}
 	}
+	private int[] CalculateLeftAndRightIndexes(ref int leftIndex, ref int rightIndex, double x1)
+	{
+		leftIndex = Math.Max(0, Math.Min(Bars.Count - 1, Chart.GetBarIndexByXCoordinate(x1)));
+		rightIndex = Bars.Count - 1;
 
+		//NOTE:  GetBarIndexByXCoordinate() returns -1 if the X coord exceeds the X of the rightmost bar
+		if (rightIndex == -1)
+		{
+			rightIndex = Bars.Count - 1;
+		}
+
+		// Gap bars are null
+		var validBarIndexes = Enumerable.Range(leftIndex, rightIndex - leftIndex)
+			.Where(i => Bars[i] != null)
+			.ToArray();
+
+		var index = leftIndex;
+		leftIndex = validBarIndexes.FirstOrDefault(k => k >= index);
+		index = rightIndex;
+		rightIndex = validBarIndexes.LastOrDefault(k => k <= index);
+		return validBarIndexes;
+	}
 	private enum VWAPIds
 	{
 		VWAP,
@@ -200,7 +188,7 @@ public sealed class AnchoredVWAP : Drawing
 	{
 		public double Multiplier { get; set; }
 		public Color Color { get; set; }
-		public int Thickness { get; set; } = 1;
+		public int Thickness { get; set; } = 0;
 		public LineStyle LineStyle { get; set; } = LineStyle.Solid;
 	}
 }
