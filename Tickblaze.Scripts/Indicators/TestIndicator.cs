33f7c47061d74dedbdbc04a3e974b0309f30b114
Jiří Běloch <belochjiri@gmail.com>
2024-11-12 13:54:34 +0100

    Add test indicator

--- Tickblaze.Scripts/Indicators/TestIndicator.cs
+++ Tickblaze.Scripts/Indicators/TestIndicator.cs
@@ -3,55 +3,64 @@ using System.Diagnostics;
 
 namespace Tickblaze.Scripts.Indicators;
 
-[Browsable(false)]
-public class TestIndicator : Indicator
+//[Browsable(false)]
+public partial class TestIndicator : Indicator
 {
-	[Plot("Bar Index")]
-	public PlotSeries Result { get; set; } = new(Color.Transparent, PlotStyle.Histogram);
+	[Plot("Plot #1")]
+	public PlotSeries Plot1 { get; set; } = new(Color.Red);
 
-	private readonly Color[] _colors = [Color.Red, Color.Green, Color.Blue, Color.White];
-	private int _index, _colorIndex;
-	private int _lastIndex = -1;
+	[Plot("Plot #2")]
+	public PlotSeries Plot2 { get; set; } = new(Color.Green);
+
+	[Plot("Plot #3")]
+	public PlotSeries Plot3 { get; set; } = new(Color.Blue);
+
+	[Parameter("Color #1")]
+	public Color Color1 { get; set; } = Color.Red;
+
+	[Parameter("Color #2")]
+	public Color Color2 { get; set; } = Color.Green;
+
+	//[Plot("Bar Index")]
+	//public PlotSeries Result { get; set; } = new(Color.Transparent, PlotStyle.Histogram);
+
+	//private readonly Color[] _colors = [Color.Red, Color.Green, Color.Blue, Color.White];
+	//private int _index, _colorIndex;
+	//private int _lastIndex = -1;
+
+	protected override void Initialize()
+	{
+		ShadeBetween(Plot1, Plot2, Color1, Color2, 0.5f);
+		ShadeBetween(Plot3, 0, Color.Yellow, Color.Blue, 0.8f);
+	}
 
 	protected override void Calculate(int index)
 	{
-		var bar = Bars[index];
+		Plot1[index] = GetSinusoidValue(index);
+		Plot2[index] = GetSinusoidValue(index, 2);
+		Plot3[index] = GetSinusoidValue(index, 0.25, 0.04);
 
-		Result[index] = bar.Close;
-		Result.Colors[index] = _colors[_colorIndex];
+		//var bar = Bars[index];
 
-		if (bar.Close > bar.Open)
-		{
-			Result.Colors[index] = Color.Green;
-		}
-        else if (bar.Close < bar.Open)
-        {
-			Result.Colors[index] = Color.Red;
-        }
-		else
-		{
-			Result.Colors[index] = Color.White;
-		}
+		//Result[index] = bar.Close;
+		//Result.Colors[index] = _colors[_colorIndex];
 
-		return;
+		//if (bar.Close > bar.Open)
+		//{
+		//	Result.Colors[index] = Color.Green;
+		//}
+		//else if (bar.Close < bar.Open)
+		//{
+		//	Result.Colors[index] = Color.Red;
+		//}
+		//else
+		//{
+		//	Result.Colors[index] = Color.White;
+		//}
+	}
 
-        if (_lastIndex > index)
-		{
-			Debugger.Break();
-		}
-
-		_lastIndex = index;
-
-		if (_index != index)
-		{
-			_index = index;
-
-			//_colorIndex = _colorIndex % 3;
-
-			if (++_colorIndex >= _colors.Length)
-			{
-				_colorIndex = 0;
-			}
-		}
+	private static double GetSinusoidValue(int index, double amplitude = 1, double frequency = 0.01, double phaseShift = 0, double verticalOffset = 0)
+	{
+		return amplitude * Math.Sin(2 * Math.PI * frequency * index + phaseShift) + verticalOffset;
 	}
 }
