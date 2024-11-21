94f052458ad4762a34256a408909196d1fe1cfa3
Jiří Běloch <belochjiri@gmail.com>
2024-11-21 17:03:14 +0100

    Create a new template

---
+++ docs/my-template/public/main.js
@@ -0,0 +1,15 @@
+export default {
+    defaultTheme: 'dark',
+    iconLinks: [
+      {
+        icon: 'github',
+        href: 'https://github.com/dotnet/docfx',
+        title: 'GitHub'
+      },
+      {
+        icon: 'twitter',
+        href: 'https://twitter.com',
+        title: 'Twitter'
+      }
+    ]
+  }
